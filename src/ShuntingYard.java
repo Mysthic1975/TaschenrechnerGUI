29ee8926b7be5a7ed6cbec8f82d60c3bbd61298f
Marcel Rohrbach <108031921+Mysthic1975@users.noreply.github.com>
2023-05-05 10:20:52 +0200

    Update ShuntingYard.java

--- src/ShuntingYard.java
+++ src/ShuntingYard.java
@@ -1,1 +1,75 @@
+import java.util.Stack;
 
+public class ShuntingYard {
+    public static Stack<String> parse(String eingabe) {
+        Stack<String> ausgabe = new Stack<>();
+        Stack<String> operatorStack = new Stack<>();
+        String[] tokens = eingabe.split(" ");
+
+        for (String token : tokens) {
+            if (isNumber(token)) {
+                ausgabe.push(token);
+            } else if (isOperator(token)) {
+                while (!operatorStack.isEmpty() && isOperator(operatorStack.peek())) {
+                    if ((isLeftAssociative(token) && getPrecedence(token) <= getPrecedence(operatorStack.peek()))
+                            || (isRightAssociative() && getPrecedence(token) < getPrecedence(operatorStack.peek()))) {
+                        ausgabe.push(operatorStack.pop());
+                        continue;
+                    }
+                    break;
+                }
+                operatorStack.push(token);
+            } else if (token.equals("(")) {
+                operatorStack.push(token);
+            } else if (token.equals(")")) {
+                while (!operatorStack.isEmpty() && !operatorStack.peek().equals("(")) {
+                    ausgabe.push(operatorStack.pop());
+                }
+                if (!operatorStack.isEmpty()) {
+                    operatorStack.pop();
+                }
+            }
+        }
+
+        while (!operatorStack.isEmpty()) {
+            ausgabe.push(operatorStack.pop());
+        }
+
+        // Drehe die Reihenfolge der Tokens
+        Stack<String> result = new Stack<>();
+        while (!ausgabe.isEmpty()) {
+            result.push(ausgabe.pop());
+        }
+
+        return result;
+    }
+
+    public static boolean isNumber(String token) {
+        try {
+            Double.parseDouble(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isOperator(String token) {
+        return isLeftAssociative(token);
+    }
+
+    private static boolean isLeftAssociative(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private static boolean isRightAssociative() {
+        return false;
+    }
+
+    private static int getPrecedence(String token) {
+        return switch (token) {
+            case "+", "-" -> 1;
+            case "*", "/" -> 2;
+            default -> 0;
+        };
+    }
+}
